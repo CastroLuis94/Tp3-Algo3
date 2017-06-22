bab21a4f252ea198aa88fcbeedf70395b57711ac
santi-alem <santialem.trev@gmail.com>
2017-06-22 12:00:33 -0300

    Generador de Tira me parece que esta andando , habria que probarlo un toque mejor . Genera un grafo segun un n , el tamaño de la tira, una clique y el tamaño de su frontera

--- generador_tira.py
+++ generador_tira.py
@@ -3,42 +3,82 @@ import random
 from rosquilla import Tupla
 
 
-def generador_tira_de_cliques(n, tam_tira, frontera_maxima):
-    g = []
-    tira = [i for i in range(1, tam_tira + 1)]
-    nodos_restantes = [i for i in range(tam_tira + 1, n + 1)]
+def frontera_clique(g, n, clique):
+    clique_aux = generar_clique(clique)
+    adyacencia = [set() for i in range(n + 1)]
+    ady_clique = [set() for i in range(n + 1)]
+    for i in g:
+        adyacencia[i.c1].add(i.c2)
+        adyacencia[i.c2].add(i.c1)
+    for i in clique_aux:
+        ady_clique[i.c1].add(i.c2)
+        ady_clique[i.c2].add(i.c1)
+    res = 0
+    for i in clique:
+        grado = len(adyacencia[i])
+        grado_clique = len(ady_clique[i])
+        res += grado - grado_clique
+    return res
 
-    random.shuffle(tira)
+
+def generador_tira_de_cliques(n, tam_tira, frontera_maxima, clique=None):
+    g = []
+    nodos = [i for i in range(1, n + 1)]
+    random.shuffle(nodos)
+
+    tam_clique = len(clique) or 4
+
+    if clique:
+        clique_aux = clique
+        for i in clique_aux:
+            nodos.remove(i)
+    else:
+        clique_aux = [nodos.pop() for _ in range(tam_clique)]
+
+    tira = random.sample(nodos, tam_tira)
+
     for i in range(tam_tira - 1):
         v = tira[i]
         w = tira[i + 1]
         g.append(Tupla(v, w))
-        tira.append(i)
-    random.shuffle(tira)
-    tam_clique = 4
 
-    clique_aux = [nodos_restantes.pop() for _ in range(tam_clique)]
+    for i in tira:
+        nodos.remove(i)
+
+    random.shuffle(tira)
 
     clique = generar_clique(clique_aux)  # Deberia ser lista de tuplas
-    for e in clique:
-        g.append(e)
-    for _ in range(frontera_maxima):
+    g += clique
+    for i in range(frontera_maxima):
         w = tira.pop()
-        v = random.choice(clique_aux)
+        v = clique_aux[(i % tam_clique)]
         g.append(Tupla(v, w))
-
-    for i in nodos_restantes:
-        v = tira.pop()
-        g.append(Tupla(v, i))
-    while nodos_restantes:
-        c = random.sample(nodos_restantes, 3)
+    cliques = []
+    while nodos:
+        tam = random.randint(1, tam_clique)
+        c = nodos[:tam]
+        clique.append(c)
         for i in c:
             for j in c:
                 if j != i:
                     g.append(Tupla(j, i))
         for i in c:
-            nodos_restantes.remove(i)
-
+            nodos.remove(i)
+        cliques.append(c)
+        frontera = random.randint(1, frontera_maxima - 1)
+        for i in range(frontera):
+            if not tira:
+                break
+            v = tira.pop()
+            w = c[i % len(c)]
+            g.append(Tupla(v, w))
+    if nodos:
+        v = nodos.pop()
+        for i in nodos[:frontera_maxima - 1]:
+            g.append(Tupla(v, i))
+    for i in cliques:
+        assert frontera_maxima > frontera_clique(g, n, i)
+    assert frontera_maxima == frontera_clique(g, n, clique_aux)
     print(str(n) + " " + str(len(g)))
     for elem in g:
         print(elem)
@@ -54,9 +94,10 @@ def generar_clique(nodos):
 
 
 if __name__ == "__main__":
-    n = 30
-    tam_tira = 15
-    cantidad_cliques = 4
-    frontera_maxima = 10
+    n = 300
+    tam_tira = 200  # mientras mas cercano sea este valor al n genera mas optimos locales valores puede generar
 
-    generador_tira_de_cliques(n, tam_tira, frontera_maxima)
+    clique = [214, 23, 14, 2, 3]
+
+    frontera_maxima = 30
+    generador_tira_de_cliques(n, tam_tira, frontera_maxima, clique)
